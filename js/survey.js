9250f98759810c836d35268f5f5b819dc29f1930
Garima Wadhwa <n01552997@humber.ca>
2022-11-29 01:01:39 -0500

    Project Phase1-About-html-css-js

---
+++ js/survey.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const $ = (selector) => document.querySelector(selector);
+
+//Display the dynamic list of validation messgaes based on the input of the data.
+const displayErrorMsgs1 = (msgs) => {
+  let display = ' ';
+  msgs.forEach((s) =>{
+  display += `<li>${s}</li>`;
+  });
+  document.getElementById('vlidationMsgs').innerHTML = display;
+};
+
+const processEntries = () => {
+  // get form controls to check for validity
+  const restaurant = $('#restaurant_address');
+  const peopleCount = $('#count');
+  const dietary = $('#food');
+  const feedback = $('#feedback');
+
+  // create array for error messages
+  const msgs = [];
+
+  // check user entries for validity
+  if (restaurant.value == '') {
+    msgs[msgs.length] = 'Please enter a restaurant name.';
+  }
+  if (peopleCount.value == '') {
+    msgs[msgs.length] = 'Please enter count of persons.';
+  }
+  if (dietary.value == '') {
+    msgs[msgs.length] = 'Please select a food type.';
+  }
+  if (feedback.value == '') {
+    msgs[msgs.length] = 'Please enter feedback.';
+  }
+
+  // submit the form or notify user of errors
+  if (msgs.length == 0) {
+    // no error messages
+    $('form').submit();
+  } else {
+    displayErrorMsgs1(msgs);
+  }
+};
+
+const resetForm = () => {
+  $('form').reset();
+
+  // remove error messages
+  $('ul').remove();
+
+  $('#restaurant_address').focus();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#submit').addEventListener('click', processEntries);
+  $('#reset_form').addEventListener('click', resetForm);
+  $('#restaurant_address').focus();
+});
