60584d5929b29c402face3b671ce515c3b017823
Gurlal Singh <gurlal.singh@certifyglobal.com>
2022-12-08 09:04:55 -0500

    Login page changes for local storage

--- js/Login.js
+++ js/Login.js
@@ -9,23 +9,28 @@ $(document).ready(()=>{
     
     });
 
-    window.onload = function () {
-        $('#loginbtn').onclick = validate;
-      };
-      
-      function validate() {
-        let username = $('#username');
-        let password = $('#password');
-        let isValid = false;
-        for (let i of credentialList){
-            if(i.username == username && i.password == password){
-                isValid = true;
-                break;
-            }
-        }
-        if(isValid){
-            window.location.href = "../html/dashboard.html";
-        }
-         
-      }
+    
  });
+
+ function validate() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let isValid = false;
+    for (let i of credentialList){
+        if(i.username == username && i.password == password){
+            isValid = true;
+            localStorage.setItem('name',i.name);
+            localStorage.setItem('email',i.username);
+            localStorage.setItem('mobile',i.mobile);
+            localStorage.setItem('gender',i.gender);
+            localStorage.setItem('language',i.language);
+            localStorage.setItem('country',i.country);
+            localStorage.setItem('dob',i.country);
+            break;
+        }
+    }
+    if(isValid){
+        window.location.href = "../html/dashboard.html";
+    }
+     
+  }
